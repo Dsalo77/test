d7b50141879da84b602fad720c1fb41db2253d92
Dsalo77 <83965923+Dsalo77@users.noreply.github.com>
2022-06-21 16:42:30 -0400

    Add files via upload

---
+++ WordBank.h
@@ -0,0 +1,12 @@
+#ifndef WORDBANK_H_
+#define WORDBANK_H_
+
+extern char *bank[] = {
+        "WINS", "LOSE", "BANK", "BIKE", "KITE",
+        "FIGHT", "MIGHT", "BEACH", "ADULT", "STACK",
+        "STRING", "FLIGHT", "ENTITY", "EMPIRE", "FOLLOW",
+        "ALCOHOL", "COLLEGE", "CAPTURE", "FICTION", "DESPITE",
+        "ONGOING", "OUTSIDE", "PACKAGE", "OVERALL", "NOTHING"
+    };
+
+#endif  // WORDBANK_H_
